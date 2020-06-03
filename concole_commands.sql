5bbb35839197b292e02720cd5aaf9a1e9dab438f
Droid008 <systemdroid007@gmail.com>
2020-06-03 04:12:39 +0300

    HomeWork Lesson №9

--- concole_commands.sql
+++ concole_commands.sql
@@ -1,61 +1,191 @@
-INSERT INTO orders (user_id)
-SELECT id FROM users WHERE name = 'Геннадий';
+SELECT * FROM shop.users;
+SELECT * FROM sample.users;
+
+START TRANSACTION;
+  INSERT INTO sample.users SELECT * FROM shop.users WHERE id = 1;
+  DELETE FROM shop.users WHERE id = 1;
+COMMIT;
+
+
+#----------------------------------------
+
+
+CREATE OR REPLACE VIEW products_catalogs AS
+SELECT
+  p.name AS product,
+  c.name AS catalog
+FROM
+  products AS p
+JOIN
+  catalogs AS c
+ON
+  p.catalog_id = c.id;
+
+
+#-------------------------------------------
 
 
 
-INSERT INTO orders_products (order_id, product_id, total)
-SELECT LAST_INSERT_ID(), id, 2 FROM products
-WHERE name = 'Intel Core i7-7700';
+CREATE TABLE IF NOT EXISTS posts (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255),
+  created_at DATE NOT NULL
+);
 
+INSERT INTO posts VALUES
+(NULL, 'первая запись', '2018-08-01'),
+(NULL, 'вторая запись', '2018-08-04'),
+(NULL, 'третья запись', '2018-08-16'),
+(NULL, 'четвертая запись', '2018-08-17');
 
+CREATE TEMPORARY TABLE last_days (
+  day INT
+);
 
-INSERT INTO orders (user_id)
-SELECT id FROM users WHERE name = 'Мария';
-
-
-
-INSERT INTO orders_products (order_id, product_id, total)
-SELECT LAST_INSERT_ID(), id, 1 FROM products
-WHERE name IN ('Intel Core i7-7700', 'asus z270h');
-
-
-
-INSERT INTO orders (user_id)
-SELECT id FROM users WHERE name = 'Иван';
-
-
-
-INSERT INTO orders_products (order_id, product_id, total)
-SELECT LAST_INSERT_ID(), id, 1 FROM products
-WHERE name IN ('AMD FX-8320', 'ASUS ROG MAXIMUS X HERO');
-
-
-
-SELECT id, name, birthday_at FROM users;
-
-
-
-SELECT u.id, u.name, u.birthday_at
-  FROM users AS u
-   JOIN orders AS o
-     ON u.id = o.user_id;
-
-     
-     
-
-     
-     
-     
----------------------------------
-
-
+INSERT INTO last_days VALUES
+(0), (1), (2), (3), (4), (5), (6), (7), (8), (9), (10),
+(11), (12), (13), (14), (15), (16), (17), (18), (19), (20),
+(21), (22), (23), (24), (25), (26), (27), (28), (29), (30);
 
 SELECT
-  p.id,
-  p.name,
-  p.price,
-  c.name AS catalog
-  FROM products AS p
-    LEFT JOIN catalogs AS c
-      ON p.catalog_id = c.id;
+  DATE(DATE('2018-08-31') - INTERVAL l.day DAY) AS day,
+  NOT ISNULL(p.name) AS order_exist
+FROM
+  last_days AS l
+LEFT JOIN
+  posts AS p
+ON
+  DATE(DATE('2018-08-31') - INTERVAL l.day DAY) = p.created_at
+ORDER BY
+  day;
 
+
+#---------------------------------------------
+
+
+
+DROP TABLE IF EXISTS posts;
+CREATE TABLE IF NOT EXISTS posts (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255),
+  created_at DATE NOT NULL
+);
+
+INSERT INTO posts VALUES
+(NULL, 'первая запись', '2018-11-01'),
+(NULL, 'вторая запись', '2018-11-02'),
+(NULL, 'третья запись', '2018-11-03'),
+(NULL, 'четвертая запись', '2018-11-04'),
+(NULL, 'пятая запись', '2018-11-05'),
+(NULL, 'шестая запись', '2018-11-06'),
+(NULL, 'седьмая запись', '2018-11-07'),
+(NULL, 'восьмая запись', '2018-11-08'),
+(NULL, 'девятая запись', '2018-11-09'),
+(NULL, 'десятая запись', '2018-11-10');
+
+DELETE
+  posts
+FROM
+  posts
+JOIN
+ (SELECT
+    created_at
+  FROM
+    posts
+  ORDER BY
+    created_at DESC
+  LIMIT 5, 1) AS delpst
+ON
+  posts.created_at <= delpst.created_at;
+
+SELECT * FROM posts;
+
+
+
+#------------------------------------------
+#По теме адиминстрирования прошелся очень поверхностно.
+
+
+CREATE USER 'shop_read'@'localhost';
+GRANT SELECT, SHOW VIEW ON shop.* TO 'shop_read'@'localhost' IDENTIFIED BY '';
+
+SELECT * FROM catalogs;
+INSERT INTO catalogs (name) VALUES ('Оперативная память');
+
+CREATE USER 'shop'@'localhost';
+GRANT ALL ON shop.* TO 'shop'@'localhost' IDENTIFIED BY '';
+
+
+
+#------------------------------------------
+
+
+
+CREATE FUNCTION hello ()
+RETURNS TINYTEXT NO SQL
+BEGIN
+  DECLARE hour INT;
+  SET hour = HOUR(NOW());
+  CASE
+    WHEN hour BETWEEN 0 AND 5 THEN
+      RETURN "Доброй ночи";
+    WHEN hour BETWEEN 6 AND 11 THEN
+      RETURN "Доброе утро";
+    WHEN hour BETWEEN 12 AND 17 THEN
+      RETURN "Добрый день";
+    WHEN hour BETWEEN 18 AND 23 THEN
+      RETURN "Добрый вечер";
+  END CASE;
+
+SELECT NOW(), hello ();
+
+
+#-----------------------------------------
+
+
+
+CREATE TRIGGER validate_name_description_insert BEFORE INSERT ON products
+FOR EACH ROW BEGIN
+  IF NEW.name IS NULL AND NEW.description IS NULL THEN
+    SIGNAL SQLSTATE '45000'
+    SET MESSAGE_TEXT = 'Both name and description are NULL';
+  END IF;
+
+INSERT INTO products
+  (name, description, price, catalog_id)
+VALUES
+  (NULL, NULL, 9360.00, 2);
+
+INSERT INTO products
+  (name, description, price, catalog_id)
+VALUES
+  ('ASUS PRIME Z370-P', 'HDMI, SATA3, PCI Express 3.0,, USB 3.1', 9360.00, 2);
+
+INSERT INTO products
+  (name, description, price, catalog_id)
+VALUES
+  (NULL, 'HDMI, SATA3, PCI Express 3.0,, USB 3.1', 9360.00, 2);
+
+CREATE TRIGGER validate_name_description_update BEFORE UPDATE ON products
+FOR EACH ROW BEGIN
+  IF NEW.name IS NULL AND NEW.description IS NULL THEN
+    SIGNAL SQLSTATE '45000'
+    SET MESSAGE_TEXT = 'Both name and description are NULL';
+  END IF;
+
+
+
+#-----------------------------------------
+
+
+
+CREATE FUNCTION FIBONACCI(num INT)
+RETURNS INT DETERMINISTIC
+BEGIN
+  DECLARE fs DOUBLE;
+  SET fs = SQRT(5);
+
+  RETURN (POW((1 + fs) / 2.0, num) + POW((1 - fs) / 2.0, num)) / fs;
+END;
+
+SELECT FIBONACCI(10);
